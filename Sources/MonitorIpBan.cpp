d98bc25b1470488d99bfcd02b60953e885ada16a
Motisma479 <robion.mathieu.16@gmail.com>
2025-04-30 23:09:46 +0200

    [sendToServer][modified]Fix

--- Sources/MonitorIpBan.cpp
+++ Sources/MonitorIpBan.cpp
@@ -12,6 +12,7 @@ MonitorIpBan::MonitorIpBan(dpp::cluster& _bot, Data& _data) : cp_bot(_bot), cp_d
     // Set the position to the end of the file initially
     log.seekg(0, std::ios::end);
     last_pos = log.tellg();
+    log.close;
 #endif
 }
 
@@ -20,7 +21,11 @@ void MonitorIpBan::Monitor()
 #if(WIN32)
     return;
 #endif
-    log.clear();
+    log.open("/var/log/fail2ban.log", std::ios::in);
+    if (!log.is_open()) {
+        std::cerr << "Failed to open log file!" << std::endl;
+        return;
+    }
     log.seekg(last_pos);
     std::string line;
     std::vector<std::string> messages;
@@ -35,21 +40,22 @@ void MonitorIpBan::Monitor()
     }
 
     last_pos = log.tellg();
+    log.close();
 
     if (messages.size() == 0) return;
 
-    int i = 0;
 
     for (const uint64_t& id : cp_data.GetIpBanListner())
     {
-        std::function<void(const dpp::confirmation_callback_t& callback)> sendRecursive = [&](const dpp::confirmation_callback_t& callback)
+        std::shared_ptr<size_t> index = std::make_shared<size_t>(0);
+        std::function<void(const dpp::confirmation_callback_t& callback)> sendRecursive = [=](const dpp::confirmation_callback_t& callback) mutable
             {
-                i++;
-                if (i >= messages.size()) return;
+                (*index)++;
+                if (*index >= messages.size()) return;
 
-                cp_bot.message_create(dpp::message(id, messages[i]), sendRecursive);
+                cp_bot.message_create(dpp::message(id, messages[*index]), sendRecursive);
             };
-        cp_bot.message_create(dpp::message(id, messages[i]), sendRecursive);
+        cp_bot.message_create(dpp::message(id, messages[*index]), sendRecursive);
     }
 
     messages.clear();
